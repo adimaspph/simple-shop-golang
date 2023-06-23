66eeb5ea560798bc3ff1a3ba2a3a58acfdba1afc
Adimas Putra Pratama <adimaspph@192-254-177-104.unifiedlayer.com>
2023-06-23 07:55:35 +0700

    add product CRUD

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -2,40 +2,125 @@ package productcontroller
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+	"github.com/jeypc/go-restapi-mux/helper"
 	"github.com/jeypc/go-restapi-mux/models"
+	"gorm.io/gorm"
 )
 
+var ResponseJson = helper.ResponseJson
+var ResponseError = helper.ResponseError
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	var products []models.Product
 
 	// if error exist
 	if err := models.DB.Find(&products).Error; err != nil {
-		fmt.Print(err)
+		ResponseError(w, http.StatusInternalServerError, err.Error())
 	}
 
-	response, _ := json.Marshal(products)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	ResponseJson(w, http.StatusOK, products)
 }
 
 func Show(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+
+	// Get ID from URL
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+
+	// Error handling
+	if err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	// Get Product by ID
+	var product models.Product
+	if err := models.DB.First(&product, id).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			ResponseError(w, http.StatusNotFound, "Product not found")
+			return
+		default:
+			ResponseError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Create(w http.ResponseWriter, r *http.Request) {
-	
+	var product models.Product
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	if err := models.DB.Create(&product).Error; err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Update(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+
+	// Get ID from URL
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+
+	// Error handling
+	if err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	var product models.Product
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&product); err != nil {
+		ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	// update product
+	if models.DB.Where("id = ?", id).Updates(&product).RowsAffected == 0 {
+		ResponseError(w, http.StatusBadRequest, "Failed to update product")
+		return
+	}
+
+	product.Id = id
+
+	ResponseJson(w, http.StatusOK, product)
 }
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	
+	inputId := map[string]int{"id" : 0}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&inputId); err != nil {
+		ResponseError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	defer r.Body.Close()
+
+	var product models.Product
+
+	if models.DB.Delete(&product, inputId["id"]).RowsAffected == 0 {
+		ResponseError(w, http.StatusBadRequest, "Failed to delete product")
+		return
+	}
+
+	message := map[string]string{"message" : "Product has been deleted successfully"}
+	ResponseJson(w, http.StatusOK, message)
 }
--- go.mod
+++ go.mod
@@ -7,10 +7,6 @@ require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/go-openapi/jsonpointer v0.19.6 // indirect
-	github.com/go-openapi/jsonreference v0.20.2 // indirect
-	github.com/go-openapi/spec v0.20.9 // indirect
-	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -19,9 +15,6 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/swaggo/files v1.0.1 // indirect
-	github.com/swaggo/http-swagger v1.3.4 // indirect
-	github.com/swaggo/swag v1.16.1 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
